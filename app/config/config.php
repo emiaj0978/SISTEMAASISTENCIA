4ed7038a826be29b33d28e90fb9f77136d4ee68f
emiaj0978 <vjgagoq.23@gmail.com>
2026-04-23 16:53:43 -0500

    AS

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+requiere_once ' config/config.php';
+?>
+<html>
+    <head>
+        <title><?php echo TITLE_BUSINESS ?></title>
+</head>
+<body>
+    <h2> Juan Garcia </h2>
---
+++ app/views/index.php
@@ -0,0 +1,14 @@
+<?php
+requiere_once ' config/config.php';
+?>
+<html>
+    <head>
+        <title><?php echo TITLE_BUSINESS ?></title>
+</head>
+<body>
+    <h2> Juan Garcia </h2>
+    <?php //abrimos php
+    //echo phpinfo
+    $nombre = "Juan";
+    $edad = 16;
+    //
